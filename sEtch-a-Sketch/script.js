b671fec970d92f367b2fb9774bdf6f46589980a5
leoemcasa <leoemcasa@hotmail.com>
2023-10-06 11:51:24 -0300

    Etch-a-Sketch with a fixed 16 squares

--- sEtch-a-Sketch/script.js
+++ sEtch-a-Sketch/script.js
@@ -7,3 +7,24 @@ for (let i = 0; i < 16 * 16; i++) {
     gridItem.classList.add('grid-item');
     gridContainer.appendChild(gridItem);
 }
+
+const gridItemS = document.querySelectorAll('.grid-item');
+gridItemS.forEach((gridItem) => {
+  
+    // and for each one we add a 'click' listener
+    gridItem.addEventListener('mouseover', () => {
+        console.log("teste ok");
+        gridItem.classList.add('grid-item-hover');
+    })
+});
+
+// const gridSquareHoverS = document.getElementsByClassName('grid-item');
+// for (let i = 0; i < gridSquareHoverS.length; i += 2) {
+//     console.log(gridSquareHoverS[i]);
+//     gridSquareHoverS[i].classList.add('grid-item-even')
+// }
+// console.log(typeof gridSquareHoverS);
+// gridSquareHoverS.array.forEach(gridSquareHover => {
+//         gridSquareHover.addEventListner('hover', alerta);
+        
+//     });
