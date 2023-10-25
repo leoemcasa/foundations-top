bfb9ad42ef188db49db2be1f54d691e5ab4b3fe9
leoemcasa <leoemcasa@hotmail.com>
2023-10-25 15:52:56 -0300

    webpack

---
+++ 03JavaScript/03webpack/webpack-htmlcssloader/src/index.js
@@ -0,0 +1,24 @@
+// import _ from 'lodash';
+// import your function
+import myName from './myName';
+
+function component() {
+  const element = document.createElement('div');
+
+  // use your function!
+  element.textContent = myName('Cody');
+  return element;
+}
+
+document.body.appendChild(component());
+
+// function componentLodashWebpack() {
+//     const element = document.createElement('div');
+  
+//     // Lodash, now imported by this script
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+//     return element;
+// }
+  
+// document.body.appendChild(componentLodashWebpack());
